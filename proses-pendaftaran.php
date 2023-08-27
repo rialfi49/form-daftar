7125fbb066b3628632433ac692ec4d055d3af76c
rialfi49 <alfiyyayusri@gmail.com>
2023-08-28 00:59:05 +0700

    first commit

---
+++ proses-pendaftaran.php
@@ -0,0 +1,103 @@
+<?php
+
+include("config.php");
+
+// cek apakah tombol daftar sudah diklik atau blum?
+if(isset($_POST['daftar'])){
+	
+	// ambil data dari formulir
+
+	$nama = $_POST['nama'];
+	$email = $_POST['email'];
+	$hp = $_POST['hp'];
+	$semester = $_POST['semester'];
+/* 	$randIpk = $_POST['ipk']; */
+	$beasiswa = $_POST['beasiswa'];
+/*   	$berkas = $_POST['berkas'];  */
+/* 2 	$proses = "Belum diverifikasi";  */
+
+
+
+$rand = rand();
+$ekstensi = array('png', 'jpg', 'jpeg');
+$filename = $_FILES['berkas']['name'];
+$ukuran = $_FILES['berkas']['size'];
+$ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+if(!in_array($ext, $ekstensi)) {
+	header("location:index.php?alert=gagal_ekstensi");
+}else{
+	if($ukuran < 1044070){
+		$xx = $rand.'_'.$filename;
+		move_uploaded_file($_FILES['berkas']['tmp_name'], 'uploads/'.$rand.'_'.$filename);
+		mysqli_query($db, "INSERT INTO register VALUES(NULL, '$nama', '$email', '$hp', '$semester', '$beasiswa', '$xx')");
+		header("location:list-pendaftar.php?alert=berhasil");
+	}else{
+		header("location:index.php?alert=gagal_ukuran");
+	}
+}
+
+
+
+
+
+/* 	$ekstensi_diperbolehkan	= array('png','jpg');
+	$namagbr = $_FILES['berkas']['name'];
+	$x = explode('.', $namagbr);
+	$ekstensi = strtolower(end($x));
+	$ukuran	= $_FILES['berkas']['size'];
+	$file_tmp = $_FILES['berkas']['tmp_name'];	
+
+		if(in_array($ekstensi, $ekstensi_diperbolehkan) === true){
+		if($ukuran < 1044070){			
+			move_uploaded_file($file_tmp, 'uploads/'.$namagbr); */
+
+/* 			$query = mysqli_query("INSERT INTO register VALUES(NULL, '$nama', '$email', '$hp', '$semester', '$beasiswa', '$namagbr')"); */
+
+/* 			$sql = "INSERT INTO register VALUES (NULL, '$nama', '$email', '$hp', '$semester', '$beasiswa', '$namagbr')"; */
+/* 			$sql = "INSERT INTO register VALUES (NULL, '$nama', '$email', '$hp', '$semester', '$beasiswa', '$namagbr')";
+			$query = mysqli_query($db, $sql); */
+
+/* 			if($query){
+				echo 'FILE BERHASIL DI UPLOAD';
+			}else{
+				echo 'GAGAL MENGUPLOAD GAMBAR';
+			}
+		}else{
+			echo 'UKURAN FILE TERLALU BESAR';
+		}
+	}else{
+		echo 'EKSTENSI FILE YANG DI UPLOAD TIDAK DI PERBOLEHKAN';
+	} */
+
+	//lolos pengecekan berkas dengan buat move upload
+/* 	     move_uploaded_file($tmpName, 'uploads/' . $namaFile);
+		 return $namaFile;
+
+    	('Location: list-pendaftar.php?status=sukses'); */
+
+
+	
+	// buat query
+/* 	$sql = "INSERT INTO register  (nama, email, hp, semester, beasiswa) VALUES ('$nama', '$email', '$hp', '$semester', '$beasiswa')";
+	$query = mysqli_query($db, $sql); */
+	
+
+	// apakah query simpan berhasil?
+/* 	if( $query ) {
+
+
+		// kalau berhasil alihkan ke halaman index.php dengan status=sukses
+		header('Location: list-pendaftar.php?status=sukses');
+	} else {
+		// kalau gagal alihkan ke halaman indek.ph dengan status=gagal
+		header('Location: index.php?status=gagal');
+	} */
+
+
+	
+} else {
+	die("Akses dilarang...");
+}
+
+?>
---
+++ ujikom.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 21, 2023 at 02:37 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ujikom`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pendaftar`
+--
+
+CREATE TABLE `ujikom` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `hp` varchar(15) NOT NULL,
+  `semester` varchar(30) NOT NULL,
+  `ipk` float NOT NULL,
+  `beasiswa` varchar(30) NOT NULL,
+  `berkas` varchar(255) NOT NULL,
+  `status` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `pendaftar`
+--
+
+INSERT INTO `pendaftar` (`id`, `nama`, `email`, `hp`, `semester`, `ipk`, `beasiswa`, `berkas`, `status`) VALUES
+(1, 'Yusri', 'Yusri@gmail.com', '000000000000', 5, 3.1, 'non_akademik', 'screenshoot23.jpg', ' belum di verifikasi'),
+(2, 'Alfiyya', 'Alfiyya@gmail.com', '11111111111', 7, 3.2, 'akademik', 'screenshoot23.jpg', 'belum di verifikasi'),
+(3, 'geulis', 'geulis@gmail.com', '1111111111', 4, 3.2, 'akademik', 'screenshoot23.jpg', 'belum di verifikasi');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pendaftar`
+--
+ALTER TABLE `pendaftar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pendaftar`
+--
+ALTER TABLE `pendaftar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
